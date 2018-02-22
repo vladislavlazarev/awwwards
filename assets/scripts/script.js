58f0aeeeb6bfdd90cfd5ee0b8c923830e3a372b9
bananaWeb <a89.volkov@gmail.com>
2018-02-22 20:45:05 +0300

    media

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 	$(".mobile-toggler").click(function(){
-	$('.header-mobile').toggleClass('-is-open');
-		
+		$('.header-mobile').toggleClass('-is-open')
 	});
 	$(".mobile-toggler").click(function(){
-	$('.mobile-toggler i').toggleClass('-cross');
-		
+		$('.mobile-toggler i').toggleClass('-cross');
 	});
+	setTimeout(function(){
+		$(".-from-above").addClass("-animated");
+		$(".-from-bottom").addClass("-animated");
+	}, 800);
 });
